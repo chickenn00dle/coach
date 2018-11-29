ed778c258ef36c2f0c773f698b58def7eedc41ef
Rasmy Nguyen <rtnguyen87@gmail.com>
2018-11-29 19:53:23 +0900

    Change color of back chevron.

--- src/components/workout/WorkoutAppBar.js
+++ src/components/workout/WorkoutAppBar.js
@@ -5,16 +5,22 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
-
+    link: {
+        color: theme.palette.primary[50],
+    }
 })
 
 const WorkoutAppBar = ({ to, title, classes }) => (
     <AppBar position="static">
         <Toolbar variant="dense">
             <IconButton>
-                <Link to={ to }>
+                <Link 
+                    to={ to } 
+                    className={ classes.link }
+                >
                     <ChevronLeft />
                 </Link>
             </IconButton>
@@ -28,4 +34,4 @@ const WorkoutAppBar = ({ to, title, classes }) => (
     </AppBar>
 )
 
-export default WorkoutAppBar
+export default withStyles( styles )( WorkoutAppBar )
